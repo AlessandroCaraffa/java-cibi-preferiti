1535b756a3118ff86be92c4876ce55fc16892936
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-17 16:07:40 +0100

    aggiunto cibo di mezzo

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -1,13 +1,22 @@
 package org.generation.italy.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CibiPreferiti {
 
 	public static void main(String[] args) {
-		String[] listaCibi = {"l'hamburgher","le lasagne","la pasta alla norma","la cotoletta","i Falafel"};
+		String[] listaCibi = {"l'hamburgher","le lasagne","la pasta alla norma","la cotoletta","il Falafel"};
+		
 		int lunghezzaLista = listaCibi.length;
+		int ciboMedio = listaCibi.length / 2;
+		
+		
 		System.out.println("La mia lista dei cibi preferiti contiene " + lunghezzaLista + " cibi.");
 		System.out.println("Il mio cibo preferito è " + listaCibi[0] + ".");
 		System.out.println("Il mio cibo preferito,ma non troppo è " + listaCibi[listaCibi.length-1] + ".");
+		System.out.println("il mio cibo di mezzo è " + listaCibi[ciboMedio] + ".");
+		
 
 	}
 
